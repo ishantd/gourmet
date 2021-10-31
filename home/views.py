5c5a07554c11644add215d0729774358344bd89c
Ishant Dahiya <ishantdahiya@gmail.com>
2021-11-01 03:56:32 +0530

    almost done

--- home/views.py
+++ home/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from recipes.models import Recipe
 
 def index(request):
-    return render(request, 'home/index.html')
+    recipes = Recipe.objects.all()[:4]
+    context = {"recipes": recipes}
+    return render(request, 'home/index.html', context)
